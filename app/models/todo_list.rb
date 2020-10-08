91f09a304bca1c7c9d55cfca83dd95d4c6c83ae0
Igor_Shum <Shumeikoigor@yandex.ru>
2020-10-08 19:52:23 +0300

    +Добавлен StimulusReflex работает на redis +Добавлено изменение состояния item'a в списке

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -13,4 +13,26 @@ class TodoList < ApplicationRecord
     return 0 if total_items == 0
     (100 * completed_items.to_f / total_items).round(1)
   end
+
+  def status
+    case percent_complete.to_i
+    when 0
+      'Не начат'
+    when 100
+      'Выполнен'
+    else
+      'В процессе'
+    end
+  end
+
+  def badge_color
+    case percent_complete.to_i
+    when 0
+      'dark'
+    when 100
+      'info'
+    else
+      'primary'
+    end
+  end
 end
